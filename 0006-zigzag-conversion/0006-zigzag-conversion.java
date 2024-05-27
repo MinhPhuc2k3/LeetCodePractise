588b09d0a6f8380658c9d9c85ebce010a7068b54
MinhPhuc2k3 <93270934+MinhPhuc2k3@users.noreply.github.com>
2024-05-28 00:16:49 +0700

    Time: 69 ms (5.29%), Space: 55.2 MB (5.15%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,29 +1,35 @@
 class Solution {
     public String convert(String s, int numRows) {
         if(numRows==1) return s;
-        int[][] arr = new int[numRows][s.length()];
-        int k=0;
-        for(int j=0; j<s.length(); j++){
-            if(j%2==0){
-                for(int i=0; i<numRows; i++){
-                    arr[i][j] = s.charAt(k);
-                    k++;
-                    if(k==s.length()) break;
+        int[][] arr =  new int[numRows][s.length()];
+        boolean state = true;
+        int row = 0;
+        int col = 0;
+        for(int i=0; i<s.length(); i++){
+            if(state || numRows ==  2){
+                arr[row][col] = s.charAt(i);
+                row++;
+                if(row==numRows) {
+                    state=false;
+                    row = numRows-2;
                 }
             }else{
-                for(int i=numRows-2; i>=1; i--){
-                    arr[i][j] = s.charAt(k);
-                    k++;
-                    if(k==s.length()) break;
+                arr[row][col] = s.charAt(i);
+                row--;
+                if(row==0){
+                    state=true;
                 }
             }
-            if(k==s.length()) break;
+            col++;
         }
         String res = "";
         for(int i=0; i<numRows; i++){
             for(int j=0; j<s.length(); j++){
                 if(arr[i][j]!=0){
-                    res += Character.toString((char) arr[i][j]);
+                    res+=Character.toString((char) arr[i][j]);
+                }
+                if(res.length()==s.length()){
+                    return res;
                 }
             }
         }
