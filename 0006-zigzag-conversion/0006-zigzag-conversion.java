48862caa95de845697da25d1cca8f4d31b84aa27
MinhPhuc2k3 <93270934+MinhPhuc2k3@users.noreply.github.com>
2024-05-28 17:24:41 +0700

    Time: 3 ms (87.76%), Space: 44.4 MB (90.36%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -2,27 +2,26 @@ class Solution {
     public String convert(String s, int numRows) {
         if(numRows==1) return s;
         int step = 2*numRows-2;
-        String res="";
+        StringBuilder res = new StringBuilder("");
         int ne = -1;
         for(int row = 0; row<numRows; row++){
             if(row==0 || row==numRows-1){
                 for(int i=row; i<s.length(); i+=step){
-                    res+=Character.toString((char) s.charAt(i));
+                    res.append(s.charAt(i));
                 }
             }else{
-                int x = ne;
-                for(int i=row; i<s.length()||x<s.length(); i+=step){
+                for(int i=row,x = ne; i<s.length()||x<s.length(); i+=step){
                     if(x>0 && x<s.length()){
-                        res+=Character.toString((char) s.charAt(x));
+                        res.append(s.charAt(x));
                     }
                     if(i>0 && i<s.length()){
-                        res+=Character.toString((char) s.charAt(i));
+                        res.append(s.charAt(i));
                     }
                     x+=step;
                 }
                 ne--;
             }
         }
-        return res;
+        return res.toString();
     }
 }
