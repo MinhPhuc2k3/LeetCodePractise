8aef18786206413757ad8c974d7651099afc6305
Lê Minh Phúc <93270934+MinhPhuc2k3@users.noreply.github.com>
2025-01-13 23:39:14 +0700

    Time: 7 ms (69.86%), Space: 18 MB (25.03%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,24 +1,38 @@
-def posMatch(s: str, p: str, i: int, j: int) -> bool:
-    if i == len(s):
-        if j==len(p):
-            return True
-        elif j+1<len(p) and p[j+1]=='*': 
-            return posMatch(s, p, i, j+2)
-        else:
-            return False
-    if j == len(p):
-        return False
-    if s[i] == p[j] or p[j] == '.':
-        if(j+1 < len(p) and p[j+1]=='*'):
-            return posMatch(s, p, i+1, j) or posMatch(s, p, i, j+2)
-        else:
-            return posMatch(s, p, i+1, j+1)
-    else:
-        if j+1 < len(p) and p[j+1] == '*':
-            return posMatch(s, p, i, j+2)
-        return False
-
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        return posMatch(s, p, 0, 0)
+        # Create a memoization dictionary
+        memo = {}
 
+        def posMatch(i: int, j: int) -> bool:
+            # Check if the result is already computed
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if i == len(s):
+                if j == len(p):
+                    memo[(i, j)] = True
+                elif j + 1 < len(p) and p[j + 1] == '*':
+                    memo[(i, j)] = posMatch(i, j + 2)
+                else:
+                    memo[(i, j)] = False
+                return memo[(i, j)]
+
+            if j == len(p):
+                memo[(i, j)] = False
+                return False
+
+            if s[i] == p[j] or p[j] == '.':
+                if j + 1 < len(p) and p[j + 1] == '*':
+                    memo[(i, j)] = posMatch(i + 1, j) or posMatch(i, j + 2)
+                else:
+                    memo[(i, j)] = posMatch(i + 1, j + 1)
+            else:
+                if j + 1 < len(p) and p[j + 1] == '*':
+                    memo[(i, j)] = posMatch(i, j + 2)
+                else:
+                    memo[(i, j)] = False
+
+            return memo[(i, j)]
+
+        # Start the recursive matching from (0, 0)
+        return posMatch(0, 0)
