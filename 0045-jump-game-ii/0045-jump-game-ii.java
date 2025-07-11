d704578bb3be45cae168955d713a91e440606340
Lê Minh Phúc <93270934+MinhPhuc2k3@users.noreply.github.com>
2025-07-11 12:43:51 +0700

    Time: 43 ms (12.79%), Space: 45.7 MB (14.24%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,8 +1,8 @@
 class Solution {
     public int jump(int[] nums) {
         int[] dp = new int[nums.length];
+        Arrays.fill(dp,1_000_000_000 );
         dp[0]=0;
-        for(int i=1; i<nums.length; i++) dp[i] = 1_000_000_000;
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<=Math.min(dp.length-1, nums[i]+i); j++){
                 dp[j] = Math.min(dp[i]+1, dp[j]);
